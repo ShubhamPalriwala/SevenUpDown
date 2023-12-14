e1d20a90cc401361abb8d9afacc8c7fb87555b0e
ShubhamPalriwala <spalriwalau@gmail.com>
2023-12-14 16:32:21 +0530

    fix: build script

--- installer.sh
+++ installer.sh
@@ -1,7 +1,16 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+# Install Rust in non-interactive mode
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
+
+# Set up the environment variables for Rust (this assumes the default installation path)
+source $HOME/.cargo/env
+
+# Add wasm target
 rustup target add wasm32-unknown-unknown
+
+# Install Soroban CLI
 cargo install --locked --version 20.0.0-rc.4.1 soroban-cli
 
-npm run build
+# Proceed with the normal build process
+react-scripts build
